e581fc7663029d06bfa96b3ee2ef4582684a60c3
unknown <morris821028@gmail.com>
2015-09-30 08:17:42 +0800

    First commit

---
+++ const.py
@@ -0,0 +1,10 @@
+WT  = 0
+CE  = 1
+OLE = 2
+MLE = 3
+RE  = 4
+TLE = 5
+WA  = 6
+AC  = 7
+res = ('Waiting', 'Compilation Error', 'Output Limit Exceeded', 'Memory Limit Exceeded', 'Runtime Error', 'Time Limit Exceeded', 'Wrong Answer', 'Accepted')
+lng = ('*', 'C99', 'C++98', 'C# 3.0', 'Python 3', 'Scala 2')
---
+++ judge_server.py
@@ -0,0 +1,64 @@
+import binascii
+import os
+import signal
+#import sqlite3
+import MySQLdb
+import subprocess
+import sys
+import time
+
+def send(ofp, lname, rname):
+	assert os.system('cd /run/shm; ln -s \'%s\' \'%s\'; tar ch \'%s\' | gzip -1 > judge_server.tgz' % (os.path.realpath(lname), rname, rname)) == 0
+	with open('/run/shm/judge_server.tgz', 'rb') as fp: b = fp.read()
+	os.remove('/run/shm/%s' % rname)
+	os.remove('/run/shm/judge_server.tgz')
+	b = binascii.hexlify(b)
+	ofp.write(('%10d' % len(b)).encode())
+	ofp.write(b)
+	ofp.flush()
+
+def work(sid, pid, lng):
+	print(sid, pid, lng, file = sys.stderr)
+	p = subprocess.Popen(['ssh', 'maplewing@140.112.30.245', 'butler'], stdin = subprocess.PIPE, stdout = subprocess.PIPE)
+	ifp = p.stdout
+	ofp = p.stdin
+	send(ofp, '../const.py', 'const.py')
+	send(ofp, '../testdata/%d/judge' % pid, 'judge')
+	if lng != 0:
+		send(ofp, '../submission/%d-0' % sid, 'source')
+	else:
+		with open('../testdata/%d/source.lst' % pid) as fp:
+			i = 0
+			for fn in fp.readlines():
+				send(ofp, '../submission/%d-%d' % (sid, i), fn[:-1])
+				i += 1
+	ofp.write(('%10d' % -lng).encode())
+	ofp.flush()
+	while True:
+		n = int(ifp.read(2))
+		if n <= 0: break
+		fn = ifp.read(n).decode()
+		send(ofp, '../testdata/%d/%s' % (pid, fn), fn)
+	score = int(ifp.readline())
+	result = int(ifp.readline())
+	cpu = int(ifp.readline())
+	mem = int(ifp.readline())
+	dtl = ifp.read()
+	if dtl:
+		with open('../submission/%d-z' % sid, 'wb') as fp: fp.write(dtl)
+	cursor.execute("UPDATE submissions SET scr=%s, res=%s, cpu=%s, mem=%s WHERE sid=%s" % (score, result, cpu, mem, sid))
+	#db.commit()
+
+def main():
+	while True:
+		cursor.execute('SELECT sid, pid, lng FROM submissions WHERE res = 0 ORDER BY sid LIMIT 1')
+		row = cursor.fetchone()
+		if row:
+			work(int(row[0]), int(row[1]), int(row[2]))
+		else:
+			time.sleep(1)
+
+assert __name__ == '__main__'
+db = MySQLdb.connect( host="140.112.xxx.xxx", user="c2015", passwd="xxxxxxxxxx", db="c2015")
+cursor = db.cursor()
+main()
