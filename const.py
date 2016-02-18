d438a925e54a8fdb6e892afc94caab035955b0f4
unknown <morris821028@gmail.com>
2016-02-18 10:54:10 +0800

    Fixed testdata script uses const PE

--- const.py
+++ const.py
@@ -6,5 +6,6 @@ RE  = 4
 TLE = 5
 WA  = 6
 AC  = 7
+PE  = -1
 res = ('Waiting', 'Compilation Error', 'Output Limit Exceeded', 'Memory Limit Exceeded', 'Runtime Error', 'Time Limit Exceeded', 'Wrong Answer', 'Accepted')
 lng = ('*', 'C99', 'C++98', 'C# 3.0', 'Python 3', 'Scala 2')
