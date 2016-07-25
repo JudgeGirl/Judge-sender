465ea64e575269c126a40be1afcc63276f70c83a
unknown <morris821028@gmail.com>
2016-07-25 14:11:16 +0800

    add config file

--- judge_server.py
+++ judge_server.py
@@ -1,12 +1,12 @@
 import binascii
 import os
 import signal
-#import sqlite3
 import pymysql
 import MySQLdb
 import subprocess
 import sys
 import time
+import yaml
 
 def send(ofp, lname, rname):
 	assert os.system('cd /run/shm; ln -s \'%s\' \'%s\'; tar ch \'%s\' | gzip -1 > judge_server.tgz' % (os.path.realpath(lname), rname, rname)) == 0
@@ -18,9 +18,9 @@ def send(ofp, lname, rname):
 	ofp.write(b)
 	ofp.flush()
 
-def work(sid, pid, lng):
+def work(sid, pid, lng, serv):
 	print(sid, pid, lng, file = sys.stderr)
-	p = subprocess.Popen(['ssh', 'butler@127.0.0.1', './butler'], stdin = subprocess.PIPE, stdout = subprocess.PIPE)
+	p = subprocess.Popen(['ssh', serv, 'butler'], stdin = subprocess.PIPE, stdout = subprocess.PIPE)
 	ifp = p.stdout
 	ofp = p.stdin
 	send(ofp, './const.py', 'const.py')
@@ -56,16 +56,32 @@ def work(sid, pid, lng):
 	cursor.execute("UPDATE submissions SET scr=%s, res=%s, cpu=%s, mem=%s WHERE sid=%s" % (score, result, cpu, mem, sid))
 	#db.commit()
 
+def prepare(sid, pid, lng):
+        address = butler_config['host']
+        account = butler_config['user']
+        try:
+                with open('../testdata/%d/server.py' % pid) as fp:
+                        info = eval(fp.read())
+                        (address, account) = (info[0][0], info[0][1])
+        except:
+                pass
+        work(sid, pid, lng, '%s@%s' % (account, address))
+
 def main():
 	while True:
 		cursor.execute('SELECT sid, pid, lng FROM submissions WHERE res = 0 ORDER BY sid LIMIT 1')
 		row = cursor.fetchone()
 		if row:
-			work(int(row[0]), int(row[1]), int(row[2]))
+			prepare(int(row[0]), int(row[1]), int(row[2]))
 		else:
-			time.sleep(1)
+			time.sleep(butler_config['period'])
 
 assert __name__ == '__main__'
-db = MySQLdb.connect( host="127.0.0.1", user="JudgeGirl", passwd="ef295a94", db="JudgeGirl")
-cursor = db.cursor()
-main()
+
+with open('_config.yml', 'r') as config_file:
+	config = yaml.load(config_file.read())
+	db_config = config['DATABASE']
+	butler_config = config['BUTLER']
+	db = MySQLdb.connect(host=db_config['host'], user=db_config['user'], passwd=db_config['password'], db=db_config['database'])
+	cursor = db.cursor()
+	main()
