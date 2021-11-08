660ffe9fa96b3eca8d0b7ea1d8a3618f5898a2c2
Daniel1147 <r08922169@ntu.edu.tw>
2021-11-08 22:33:39 +0800

    Add a script for sending style check tasks by pid.

---
+++ scripts/send_style_check_task/send_by_pid.py
@@ -0,0 +1,33 @@
+import sys
+import traceback
+from judge_common import DB, Logger, Config
+
+from helper import send_tasks
+
+
+def print_usage():
+    print(f"usage: python {sys.argv[0]} <<pid>>")
+
+
+def get_sid_list(pid: str):
+    config = Config("./_config.yml")
+    db = DB(config)
+
+    template = "SELECT sid FROM submissions WHERE pid = %s"
+    parameter_tuple = (pid,)
+    result = db._query_all(template, parameter_tuple)
+
+    return [row["sid"] for row in result]
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print_usage()
+        exit(1)
+
+    pid = sys.argv[1]
+    try:
+        sid_list = get_sid_list(pid)
+        send_tasks(sid_list)
+    except Exception as e:
+        Logger.error(traceback.format_exc())
